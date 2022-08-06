f7b779032f8ca5e00756aed65c5440de5a6a5894
LynnCrescent <azureplayers@gmail.com>
2022-08-06 10:36:07 +0700

    first commit

---
+++ Index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title> DevLynn </title>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="Stylesheet" type="text/css" href="Assets/Css/Style.css">
+     <link rel="Icon" href="Assets/Img/85629-league-legends-samsung-of-screen-wallpaper-character.png">
+     <link href="http://fonts.cdnfonts.com/css/mars-landing" rel="stylesheet">
+</head>
+<body>
+
+     <div class="Navigation-Container">
+          <div class="Navigation-Stick-1"></div>
+               <div class="Navigation-Top-Center">
+                    <label>
+                         Lynn
+                    </label>
+               </div>
+               <div class="Navigation-Stick">
+          </div>
+     </div>
+
+     <div class="Landing-Page">
+          <div class="Landing-Content">
+               <div class="Reminder">
+                    <div class="Header">
+                         <p>
+                              Landing Page
+                         </p>
+                    </div>
+               </div>
+               <div class="Image-Content">
+                    <img src="Assets/Img/Wallpaper.png">
+               </div>
+               <div class="Side-Bar">
+                    <div class="Bar-1">
+                         <div class="Label-Logo-1">
+                              <label>
+                                   Developer
+                              </label>
+                         </div>
+                    </div>
+               </div>
+               <div class="Theme-Container">
+                    <div class="Theme-Text">
+                         <label>
+                              GAMING
+                         </label>
+                    </div>
+                    <div class="Theme-Text-2">
+                         <label>
+                              THEME 
+                         </label>
+                    </div>
+               </div>
+               <div class="Line-1"></div>
+               <div class="Line-2"></div>
+               <div class="Text-Information-Container">
+                    <div class="Text-Information">
+                         <label>
+                              Lynn
+                         </label>
+                    </div>
+                    <div class="Short-Information">
+                         <h4>
+                              Join Game Developer
+                         </h4>
+                         <p>
+                              " Be A Game Developer Isn't That Hard If You Use DevLynn, DevLynn it's A Website That Will Help You To Improve Your Game "
+                         </p>
+                    </div>
+                    <div class="Button-Login">
+                         <a href="">
+                              Join Now
+                         </a>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+     <div class="Center-Text">
+          <div class="Label-Text-Center-1">
+               <label>
+                    Learn
+               </label>
+          </div>
+          <div class="Label-Text-Center-2">
+               <label>
+                    More!
+               </label>
+          </div>
+     </div>
+
+     <div class="Block-Center-Content">
+          <div class="Lines-Container">
+               <div class="Line-3"></div>
+          </div>
+          <div class="Content-Center">
+               <div class="Image-Display">
+                    <img src="Assets/Img/Wallapaper Display.png">
+               </div>
+               <div class="About-Content">
+                    <div class="Text-About-Label">
+                         <label>
+                              About
+                         </label>
+                    </div>
+                    <div class="Text-About-h2">
+                         <h2>
+                              DevLynn
+                         </h2>
+                    </div>
+                    <div class="Text-About-P">
+                         <p>
+                              
+                         </p>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+</body>
